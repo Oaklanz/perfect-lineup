32b85631e31851d99f5bb8f01d88f6dea834ff8b
Oaklanz <69703980+Oaklanz@users.noreply.github.com>
2021-04-18 19:56:36 -0400

    created a function that resolves total salay

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-const validateLineup = () => {
+const validateLineup = (lineup) => {
+    
+    //returns false when the lineup has a total salary greater than 45000'
+    
+    const totalSalary = (lineup) => lineup.map(lineup.salary).reduce((accm, val) => accm + val, 0)
+        if (totalSalary > 45,000){return false}
+    
+    // //returns false when lineup includes too many players from a single team
+    // //loop through array
+    // //if 
+    // const ManyPlayers = (lineup) =>{
+    //      lineup.map(lineup => lineup.teamId) 
+                                             
+    //                                 }
+                                
+
+    // //returns false when lineup includes too many players from a single game
+
+    //  const lineupincludesTooManyPlayers = () => {return false
+
+    // }
+    // //returns false when lineup includes too many players
+    // //eturns false when the lineup does not have the right number of players at each position'
+    // //returns false when the lineup includes too few players from a single position'
+    // //returns false when the lineup does not include a player from a position'
+    
+    // // //returns true when the lineup satisfies all conditions
     return true
 } 
+
 
 module.exports = validateLineup
