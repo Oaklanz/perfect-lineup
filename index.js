20006940d522b1025e0fa14d7632dc0fa492a4ff
Oaklanz <69703980+Oaklanz@users.noreply.github.com>
2021-04-20 00:52:13 -0400

    perfect lineup assignment

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const validateLineup = (lineup) => {
-    
-    //returns false when the lineup has a total salary greater than 45000'
-    
-    const totalSalary = (lineup) => {lineup.map(lineup.salary).reduce((accm, val) => accm  + val, 0)
+    let positionCounts = getPositionCount(lineup) && 
+    let positionCount =  fewPositionCount(lineup) &&
+    let positionCount = includePositionCount(lineup) &&
+    let totalSalary > 45,000
+
+    const totalSalary = (lineup) => {
+        lineup.map(lineup.salary).reduce((acc, val) => acc  + val, 0)
         if (totalSalary > 45,000)
         return false 
     }
-    
-    //returns false when lineup includes too many players from a single team
     
     const ManyPlayers = (lineup) =>{
          lineup.map(lineup => lineup.teamId) 
@@ -21,8 +22,6 @@ const validateLineup = (lineup) => {
              return ManyPlayers, false; 
 
            }
-                                
-    //returns false when lineup includes too many players from a single game
 
      const singleGame = (lineup) => {lineup.map(lineup => lineup.gameId)
 
@@ -36,8 +35,7 @@ const validateLineup = (lineup) => {
              return singleGame, false
 
     }
-    //returns false when lineup includes too many players
-
+    
      const numberOfPlayers = (lineup) => {
          lineup.map(lineup.id).reduce((accm, val) => accm  + val, 0)
 
@@ -46,12 +44,39 @@ const validateLineup = (lineup) => {
      }
 
 
+        const singlePosition = (position) => { return position['P'] === 1 || ['C'] === 1 || ['3B'] === 1 || ['1B'] === 1 || ['2B'] === 1 || ['SS'] === 1 || ['OF'] === 3 }
+        
+        const getPositionCount = (lineup) => {
+            return lineup.reduce((counts, player) => { 
+                counts[player.position] = counts[player.position] === undefined ? 1 : counts[player.position] + 1
+            
+            return counts 
+        }, {} )  
+        }
 
-    //eturns false when the lineup does not have the right number of players at each position'
-    //returns false when the lineup includes too few players from a single position'
-    //returns false when the lineup does not include a player from a position'
+
+        const fewPlayers = (position) => { return position['P'] === 1 || ['SS'] === 1 || ['C'] === 1 || ['1B'] === 1 || ['2B'] === 1 || ['3B'] === 1 || ['OF'] === 3 }
+
+        const fewPositionCount = (lineup) => {
+            return lineup.reduce((counts, player) => {
+                counts[player.position] = counts[player.position] === undefined ? 1 : counts[player.position] + 1
+
+            return counts    
+        }, {} )
+         }
+
+
+        const includePlayerPositionn = (position) => { return position['1B'] === 1 || ['2B'] === 1 || ['3B'] === 1 || ['P'] === 1 || ['SS'] === 1 || ['OF'] === 3 }
+
+        const includePositionCount = (lineup) => {
+            return lineup.reduce((counts, player) => {
+                counts[player.position] = counts[player.position] === undefined ? 1 : counts[player.position] + 1
+
+            return counts    
+        }, {} )
+         }
     
-    // //returns true when the lineup satisfies all conditions
+
     return true
 } 
 
